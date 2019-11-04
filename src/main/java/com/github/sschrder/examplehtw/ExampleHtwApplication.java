3271762594df583b9eb1536cbd5752f5c0767363
Laura <laurazaworski@yahoo.de>
2019-11-04 12:26:59 +0100

    test

---
+++ src/main/java/com/github/sschrder/examplehtw/ExampleHtwApplication.java
@@ -0,0 +1,13 @@
+package com.github.sschrder.examplehtw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExampleHtwApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExampleHtwApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/sschrder/examplehtw/ExampleHtwApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.sschrder.examplehtw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExampleHtwApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
